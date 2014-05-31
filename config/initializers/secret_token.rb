4a6abf6e5a4d2ebf659c068fd4b9cc716518abd0
Roberto Fernandez <robertofernandezvillarreal@gmail.com>
2014-05-31 14:19:10 -0430

    RoR:Curso

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cursoo::Application.config.secret_key_base = '32ac94b0c8a7b7f8ef48e254a4abeabfa51d4404fbc27a5ce4300504209267459fdd1835391f3a270154c6fbebea29ce94abe6ad343dff6f851b8e4f77e142fa'
