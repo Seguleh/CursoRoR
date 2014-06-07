cd35e08af21e14f18f6fa95e02647425cf24ef37
Roberto Fernandez <robertofernandezvillarreal@gmail.com>
2014-06-07 12:06:38 -0430

    Clase 3

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.0.0'
 # Use postgresql as the database for Active Record
 gem 'pg'
 
+#Something fantastic
+gem 'formtastic'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,39 @@
 class UsersController < ApplicationController
 	def index
-		 @users = User.all
+		@users = User.all
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		if User.create(permit_params)
+			flash[:notice] = "User successfully made"
+			redirect_to users_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(permit_params)
+			flash[:notice] = "User successfully edited"
+			redirect_to users_path
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+	def permit_params
+		params.require(:user).permit!
 	end
 end
